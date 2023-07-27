890149649428c63e00eefee0d8f3862983ab30be
cpapon <ctgpapon@gmail.com>
2023-07-27 23:05:46 +0600

    finaly ready for submit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+ let mobile_icon = document.getElementById("mobile_icon");
+ let mobile_icon = document.getElementById("small_menu");
+
+ mobile_icon.addEventListener("click",()=>{
+     small_menu.classList.toggle('display_menu')
+ })
+
+
