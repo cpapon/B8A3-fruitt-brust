008407994d913ce752693e701c509ccad37d6bb1
cpapon <ctgpapon@gmail.com>
2023-07-27 23:17:26 +0600

    trying mobile menu responsive

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
  let mobile_icon = document.getElementById("mobile_icon");
- let mobile_icon = document.getElementById("small_menu");
+ let small_menu = document.getElementById("small_menu");
 
  mobile_icon.addEventListener("click",()=>{
      small_menu.classList.toggle('display_menu')
